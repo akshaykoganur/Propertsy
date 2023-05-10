7f8fb4a540cffa88e2385af7ae37907b57eaea2e
akshaykoganur <akshaykoganur@gmail.com>
2023-05-10 23:40:42 +0530

    commit

--- backend/routes/displayBuy.js
+++ backend/routes/displayBuy.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const sellModel = require('../models/sellPropertyModel')
 
-router.get('/buyData', async(req,res) => {
+router.post('/buyData', async(req,res) => {
     try{
-        const buyData = await sellModel.find({});
+        const buyData = await sellModel.find({city : req.body.city});
         //console.log(buyData);
         res.send(buyData);
     }
--- frontend/src/pages/Buy.js
+++ frontend/src/pages/Buy.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from "react";
+
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
 //import books2 from "./images/books2.jpg";
 import "../index.css";
 import BuyCard from "./BuyCard";
 
 function Buy() {
   const [dat, setMyArray] = useState([]);
+  const [city, setCity] = useState('');
   //const clicked = async (e) => {
-    //console.log('Hi');
+  //console.log('Hi');
   //}
-  async function getBuyProperties() {
+  async function submitData() {
     try {
       let url = "http://localhost:5000/api/buyData";
-      let res = await fetch(url);
+      let res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          city
+        })
+      });
       let data = await res.json();
       console.log(data);
       setMyArray(data);
@@ -19,9 +30,12 @@ function Buy() {
       console.error(err);
     }
   }
-  useEffect(() => {
-    getBuyProperties();
-  }, []);
+
+  const changed = async(e) => {
+    let value = e.target.value;
+    setCity(value);
+}
+
   return (
     <>
       {/* <div className="container mb-3 fs-3 m-3">
@@ -40,20 +54,33 @@ function Buy() {
         )}
       </div> */}
 
-<div className="container buy mb-3 fs-3 d-flex flex-wrap">
-  {dat !== [] ? (
-    dat.map((data, idx) => {
-      return (
-        <div key={idx} className="col-12 col-md-6 col-lg-3 mx-5">
-          <BuyCard buyProperties={data} />
+      <div className="authentication">
+        <div className="authentication-form card p-2">
+          <h1 className='card-title'>Enter City</h1>
+          <Form method="POST" layout='vertical' onFinish={submitData}>
+            <Form.Item label='city' >
+              <Input name='city' placeholder='City' value={city} onChange={changed} />
+            </Form.Item>
+            <Button className='primary-button mt-2 mb-2' htmlType='submit'>REGISTER</Button>
+          </Form>
+
         </div>
-      );
-    })
-  ) : (
-    <div>**</div>
-  )}
-</div>
-      
+      </div>
+
+      <div className="container buy mb-3 fs-3 d-flex flex-wrap">
+        {dat !== [] ? (
+          dat.map((data, idx) => {
+            return (
+              <div key={idx} className="col-12 col-md-6 col-lg-3 mx-5">
+                <BuyCard buyProperties={data} />
+              </div>
+            );
+          })
+        ) : (
+          <div>**</div>
+        )}
+      </div>
+
     </>
   );
 }
