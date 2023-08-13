f778af4cd81c0bb4aaa1fca0474bef31223a5e3c
akshaykoganur <akshaykoganur@gmail.com>
2023-08-13 19:06:36 +0530

    fixed rentData

--- backend/routes/displayRent.js
+++ backend/routes/displayRent.js
@@ -4,7 +4,7 @@ const rentModel = require('../models/rentPropertyModel')
 
 router.get('/rentData', async(req,res) => {
     try{
-        const buyData = await rentModel.find({city : req.body.city, type: req.body.type});
+        const rentData = await rentModel.find({city : req.body.city, type: req.body.type});
         res.send(rentData);
     }
     catch(error){
