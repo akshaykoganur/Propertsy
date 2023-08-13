264fef763d16149a9ef83ec500bee0a1654fc7cc
akshaykoganur <akshaykoganur@gmail.com>
2023-08-13 19:20:16 +0530

    Corrected rent API request

--- backend/routes/displayRent.js
+++ backend/routes/displayRent.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const rentModel = require('../models/rentPropertyModel')
 
-router.get('/rentData', async(req,res) => {
+router.post('/rentData', async(req,res) => {
     try{
         const rentData = await rentModel.find({city : req.body.city, type: req.body.type});
         res.send(rentData);
