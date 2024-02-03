0fc1c644ffbf0182e272c1ab76c2ffcf430e16d8
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:07:36 +0530

    Update server.js with PostgreSQL

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,37 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 require('dotenv').config();
-const dbconfig = require('./config/dbConfig');
+//const dbconfig = require('./config/dbConfig');
 
 const displayBuy = require('./routes/displayBuy');
 const displayRent = require('./routes/displayRent');
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true , parameterLimit:50000}));
+
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});  
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message); 
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 
 const userRoute = require('./routes/userRoute');
 app.use('/api/user', userRoute);
@@ -45,4 +69,4 @@ app.use((req, res, next) => {
 
 const port = 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
