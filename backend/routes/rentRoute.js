e82e0834147755c5a21c4fc2124f91151ad3e946
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:03:47 +0530

    Update rentRoute.js with PostgreSQL

--- backend/routes/rentRoute.js
+++ backend/routes/rentRoute.js
@@ -5,8 +5,22 @@ const bcrypt = require('bcryptjs');
 
 router.post('/rent', async (req, res) => {
     try{
-        const newrent = new Rent(req.body);  //create new user
-        await newrent.save();     //save new user
+        const { name, type, image, rent, deposit, street, locality, city, ownerName, ownerContact, ownerEmail } = req.body;
+        await prisma.rent.create({
+            data:{
+              name: name,
+              type: type,
+              image: image,
+              rent: rent,
+              deposit: deposit,
+              street: street,
+              locality: locality,
+              city: city,
+              ownerName: ownerName,
+              ownerContact: ownerContact,
+              ownerEmail: ownerEmail,
+            },
+          });
         res.status(200).send({message: "Property added", success: true});
     }
     catch(error){
