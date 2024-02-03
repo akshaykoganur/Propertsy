7346f85f4fda77e02123e1b627f36892d92c4f72
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:05:18 +0530

    Update displayRent.js with PostgreSQL

--- backend/routes/displayRent.js
+++ backend/routes/displayRent.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const rentModel = require('../models/rentPropertyModel')
+const prisma_client = require('@prisma/client');
+const prisma = new prisma_client.PrismaClient();
 
 router.post('/rentData', async(req,res) => {
     try{
-        const rentData = await rentModel.find({city : req.body.city, type: req.body.type});
+        const buyData = await prisma.rent.findMany({ where: {city : req.body.city, type: req.body.type},});
         res.send(rentData);
     }
     catch(error){
