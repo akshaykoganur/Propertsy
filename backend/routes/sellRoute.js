7f12819aebf2e8f0fac904205feec2148642e8bf
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:03:05 +0530

    Update sellRoute.js with PostgreSQL

--- backend/routes/sellRoute.js
+++ backend/routes/sellRoute.js
@@ -2,11 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Sell = require('../models/sellPropertyModel');
 const bcrypt = require('bcryptjs');
+const prisma_client = require('@prisma/client');
+const prisma = new prisma_client.PrismaClient()
 
 router.post('/sell', async (req, res) => {
     try{
-        const newSell = await Sell.create(req.body);
-        newSell.save(); //save new user
+        const { name, type, image, price, age, street, locality, city, ownerName, ownerContact, ownerEmail } = req.body;
+        await prisma.sell.create({
+            data:{
+              name: name,
+              type: type,
+              image: image,
+              price: price,
+              age: age,
+              street: street,
+              locality: locality,
+              city: city,
+              ownerName: ownerName,
+              ownerContact: ownerContact,
+              ownerEmail: ownerEmail,
+            },
+          });
         res.status(200).send({message: "Property added", success: true});
     }
     catch(error){
