a4288b8d21a8e5b3d13e329f397223640520da6e
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:04:41 +0530

    Update displayBuy.js with PostgreSQL

--- backend/routes/displayBuy.js
+++ backend/routes/displayBuy.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const sellModel = require('../models/sellPropertyModel')
+const prisma_client = require('@prisma/client');
+const prisma = new prisma_client.PrismaClient();
 
 router.post('/buyData', async(req,res) => {
     try{
-        const buyData = await sellModel.find({city : req.body.city, type: req.body.type});
+        const buyData = await prisma.sell.findMany({ where: {city : req.body.city, type: req.body.type},});
         //console.log(buyData);
         res.send(buyData);
     }
