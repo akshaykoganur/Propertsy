6ed85be1eaabb524a474e68af275786ace28b4bc
Akshay Koganur <90169834+akshaykoganur@users.noreply.github.com>
2024-02-03 10:02:10 +0530

    Update userRoute.js with PostgreSQL

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,40 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
+//const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authMiddleware = require('../middleware/authMiddleware');
+const prisma_client = require('@prisma/client');
+const prisma = new prisma_client.PrismaClient()
 
 router.post('/register', async (req, res) => {
   try {
-    const userExists = await User.findOne({ email: req.body.email });  //get the mail id to check if account on same mail id exists
+    const userExists = await prisma.user.findUnique({ where: { email: req.body.email }, });  //get the mail id to check if account on same mail id exists
     if (userExists) {
       return res.status(400).send({ message: "User already exists", success: false }); //send the error that user with mail id already exists and we will not create another user with same id
     }
-    const password = await req.body.password;     //to get the password
+    const { name, email, contactno, password } = req.body;
     const salt = await bcrypt.genSalt(10);   //bcrpyt password
     const hashedPassword = await bcrypt.hash(password, salt);
     req.body.password = hashedPassword;
-    const newuser = new User(req.body);  //create new user
-    await newuser.save();     //save new user
+    await prisma.user.create({
+      data:{
+        email: email,
+        name: name,
+        contactno: contactno,
+        password: hashedPassword,
+      },
+    });  //create new user
+    console.log(email);
     res.status(200).send({ message: "User created successfully", success: true }); //send message when user is created successfully
   } catch (error) {
     res.status(500).send({ message: "Error creating message", success: false });
   }
 })
 
-router.post('/deleteuser', async (req, res) => {
+/*router.post('/deleteuser', async (req, res) => {
   try {
     const user = await UserModal.findByIdAndRemove(req.body.email);
     res.status(200).send({ message: "User deleted", success: true });
   } catch (error) {
     res.status(500).send({ message: "Error deleting message", success: false });
   }
-})
+})*/
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await prisma.user.findUnique({ where: { email: req.body.email }, });  //get the mail id to check if account on same mail id exists
     if (!user) {
       return res
         .status(200)
@@ -63,7 +72,7 @@ router.post("/login", async (req, res) => {
 
 router.post('/profile', async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await prisma.user.findOne({ email: req.body.email });
     //user.password = undefined;
     if (!user) {
       return res
